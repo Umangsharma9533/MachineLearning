6b6f702540e88d797e5a4ec6898fdee98f32e929
Umangsharma9533 <40338157+Umangsharma9533@users.noreply.github.com>
2020-07-03 02:30:44 +0530

    Update AdvancedOperations_ImageProcessing.py

---
+++ MachineAndDeepLearning/ImageProcessingWithPython/AdvancedOperations_ImageProcessing.py
@@ -0,0 +1,106 @@
+# Import the canny edge detector 
+from skimage.feature import canny
+
+# Convert image to grayscale
+grapefruit = color.rgb2gray(grapefruit)
+
+# Apply canny edge detector
+canny_edges = canny(grapefruit)
+
+# Show resulting image
+show_image(canny_edges, "Edges with Canny")
+
+#==========================================
+#More sigma less edges
+
+# Apply canny edge detector with a sigma of 1.8
+edges_1_8 = canny(grapefruit, sigma=1.8)
+
+# Apply canny edge detector with a sigma of 2.2
+edges_2_2 = canny(grapefruit, sigma=2.2)
+
+# Show resulting images
+show_image(edges_1_8, "Sigma of 1.8")
+show_image(edges_2_2, "Sigma of 2.2")
+
+#==============================================
+# Import the corner detector related functions and module
+from skimage.feature import corner_harris, corner_peaks
+
+# Convert image from RGB-3 to grayscale
+building_image_gray = color.rgb2gray(building_image)
+
+# Apply the detector  to measure the possible corners
+measure_image = corner_harris(building_image_gray)
+
+# Find the peaks of the corners using the Harris detector
+coords = corner_peaks(measure_image, min_distance=2)
+
+# Show original and resulting image with corners detected
+show_image(building_image, "Original")
+show_image_with_corners(building_image, coords)
+
+# Find the peaks with a min distance of 2 pixels
+coords_w_min_2 = corner_peaks(measure_image, min_distance=2)
+print("With a min_distance set to 2, we detect a total", len(coords_w_min_2), "corners in the image.")
+
+# Find the peaks with a min distance of 40 pixels
+coords_w_min_40 = corner_peaks(measure_image, min_distance=40)
+print("With a min_distance set to 40, we detect a total", len(coords_w_min_40), "corners in the image.")
+
+# Show original and resulting image with corners detected
+show_image_with_corners(building_image, coords_w_min_2, "Corners detected with 2 px of min_distance")
+show_image_with_corners(building_image, coords_w_min_40, "Corners detected with 40 px of min_distance")
+
+
+#===================================================================
+# Load the trained file from data
+trained_file = data.lbp_frontal_face_cascade_filename()
+
+# Initialize the detector cascade
+detector = Cascade(trained_file)
+
+# Detect faces with scale factor to 1.2 and step ratio to 1
+detected = detector.detect_multi_scale(img=friends_image,
+                                       scale_factor=1.2,
+                                       step_ratio=1,
+                                       min_size=(10, 10),
+                                       max_size=(200, 200))
+# Show the detected faces
+show_detected_face(friends_image, detected)
+
+#===================================================================
+#Privacy Protection
+# Detect the faces
+detected = detector.detect_multi_scale(img=group_image,
+scale_factor=1.2, step_ratio=1,
+min_size=(10, 10), max_size=(100, 100))
+# For each detected face
+for d in detected:  
+    # Obtain the face rectangle from detected coordinates
+    face = getFaceRectangle(d)
+    
+    # Apply gaussian filter to extracted face
+    blurred_face = gaussian(face, multichannel=True, sigma = 8)
+    
+    # Merge this blurry face to our final image and show it
+    resulting_image = mergeBlurryFace(group_image, blurred_face) 
+show_image(resulting_image, "Blurred faces")
+
+
+
+# Import the necessary modules
+from skimage.restoration import denoise_tv_chambolle,inpaint
+from skimage import transform
+
+# Transform the image so it's not rotated
+upright_img = transform.rotate(damaged_image, 20)
+
+# Remove noise from the image, using the chambolle method
+upright_img_without_noise = denoise_tv_chambolle(upright_img,weight=0.1, multichannel=True)
+
+# Reconstruct the image missing parts
+mask = get_mask(upright_img)
+result = inpaint.inpaint_biharmonic(upright_img_without_noise, mask, multichannel=True)
+
+show_image(result)
