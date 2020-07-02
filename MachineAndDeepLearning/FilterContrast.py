d9f2c5c99aa3f6f340b897b17a6c2bd771234aa9
Umang Sharma <40338157+Umangsharma9533@users.noreply.github.com>
2020-07-02 11:27:39 +0530

    Update FilterContrast.py

--- MachineAndDeepLearning/FilterContrast.py
+++ MachineAndDeepLearning/FilterContrast.py
@@ -17,3 +17,15 @@ edge_sobel = sobel(soaps_image_gray)
 # Show original and resulting image to compare
 show_image(soaps_image, "Original")
 show_image(edge_sobel, "Edges with Sobel")
+
+
+#===================================================
+# Import Gaussian filter 
+from skimage.filters import gaussian
+
+# Apply filter
+gaussian_image = gaussian(building_image,multichannel=True)
+
+# Show original and resulting image to compare
+show_image(building_image, "Original")
+show_image(gaussian_image, "Reduced sharpness Gaussian")
