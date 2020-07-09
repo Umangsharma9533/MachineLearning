2017d24ae47e6e676242404e61dcd6bd8b4787bb
Umangsharma9533 <40338157+Umangsharma9533@users.noreply.github.com>
2020-07-09 23:20:50 +0530

    Create Introduction_Tensorflow.py

---
+++ MachineAndDeepLearning/Introduction_To_Tensorflow_In_Python/Introduction_Tensorflow.py
@@ -0,0 +1,28 @@
+# Import constant from TensorFlow
+from tensorflow import constant
+
+# Convert the credit_numpy array into a tensorflow constant
+credit_constant = constant(credit_numpy)
+
+# Print constant datatype
+print('The datatype is:', credit_constant.dtype)
+
+# Print constant shape
+print('The shape is:', credit_constant.shape)
+
+#==================================
+# Define the 1-dimensional variable A1
+A1 = Variable([1, 2, 3, 4])
+
+# Print the variable A1
+print(A1)
+
+# Convert A1 to a numpy array and assign it to B1
+B1 = A1.numpy()
+
+# Print B1
+print(B1)
+
+
+#=========================================
+#Element wise multiplication
